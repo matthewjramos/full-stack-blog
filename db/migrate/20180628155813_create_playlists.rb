ce8b2115a1346eba3154409d28b86e4eb1751174
matthewjramos <ramos.matthew.jorge@gmail.com>
2018-06-28 12:12:51 -0400

    Create database, scaffold, migrate
    Create database with db:create
    Create playlist resource with scaffold and migration

---
+++ db/migrate/20180628155813_create_playlists.rb
@@ -0,0 +1,11 @@
+class CreatePlaylists < ActiveRecord::Migration[5.1]
+  def change
+    create_table :playlists do |t|
+      t.string :title
+      t.string :description
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
